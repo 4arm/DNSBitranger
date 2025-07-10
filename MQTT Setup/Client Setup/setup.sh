2279ed016d478b56bd75ec86e562f4c25581c5ff
4arm <91441819+4arm@users.noreply.github.com>
2025-07-10 16:02:25 +0800

    Update setup.sh

--- MQTT Setup/Client Setup/setup.sh
+++ MQTT Setup/Client Setup/setup.sh
@@ -16,6 +16,9 @@ cd "$INSTALL_DIR"
 # Download the publisher script
 curl -O https://raw.githubusercontent.com/4arm/DNSBitranger/refs/heads/main/MQTT%20Setup/Client%20Setup/mqtt_publisher.py
 
+# Download the nxdomain sender
+curl -O https://raw.githubusercontent.com/4arm/DNSBitranger/refs/heads/main/MQTT%20Setup/Client%20Setup/nxdomain_sender.py
+
 # Install required Python packages system-wide
 pip3 install --break-system-packages paho-mqtt requests
 
@@ -24,6 +27,9 @@ chmod +x mqtt_publisher.py
 
 # Set up cron job (if not already there)
 cron_entry="*/5 * * * * /usr/bin/python3 $INSTALL_DIR/mqtt_publisher.py"
+(crontab -l 2>/dev/null | grep -Fv "$cron_entry"; echo "$cron_entry") | crontab -
+
+cron_entry="*/5 * * * * /usr/bin/python3 $INSTALL_DIR/nxdomain_sender.py"
 (crontab -l 2>/dev/null | grep -Fv "$cron_entry"; echo "$cron_entry") | crontab -
 
 echo "[✓] DNS client setup complete. Publishes to MQTT every 5 minutes."
